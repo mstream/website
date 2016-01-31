26b21825e22c36116675fb89fc87ca950902d6b0
Maciej Laciak <maciej.laciak@gmail.com>
2016-01-31 23:23:14 +0000

    Creates a posts list layout.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,27 +2,69 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Post {
-    constructor(title) {
+    constructor(id, title, summary, dateCreated) {
+        this.id = id;
         this.title = title;
+        this.summary = summary;
+        this.dateCreated = dateCreated;
     }
 }
 
-const posts = [new Post("aaa"), new Post("bbb"), new Post("ccc")];
+const summary = ((amount) => {
+    const generatedSummary = [];
+    for (let i = 1; i <= amount; i++) {
+        generatedSummary.push("summary");
+    }
+    return generatedSummary.join(" ");
+})(50);
+
+const posts = ((amount) => {
+    const generatedPosts = [];
+    for (let i = 1; i <= amount; i++) {
+        generatedPosts.push(new Post(i, `Post #${i}`, summary, new Date()));
+    }
+    return generatedPosts;
+})(100);
 
 class PostsList extends React.Component {
     render() {
-        const posts = this.props.posts.map((post) => <PostHeader title={post.title}/>);
-        return <ul>{posts}</ul>;
+        const posts = this.props.posts.map((post) =>
+            <PostHeader key={post.id} post={post}/>);
+        return (<ul className="list-unstyled list-group">{posts}</ul>);
     }
 }
 
 class PostHeader extends React.Component {
     render() {
-        return <li>{this.props.title}</li>
+        const {title, summary, dateCreated} = this.props.post;
+        return (
+            <li>
+                <a href="#" className="list-group-item">
+                    <article className="media">
+                        <figure className="media-left media-top">
+                            <img
+                                src="http://lorempixel.com/image_output/technics-q-c-240-240-4.jpg"/>
+                        </figure>
+                        <div className="media-body">
+                            <header>
+                                <h2>{title}</h2>
+                            </header>
+                            <p>{summary}</p>
+                            <footer>
+                                <h6 className="text-right">
+                                    <em>Created: <strong><time>{dateCreated.toDateString()}</time></strong>
+                                    </em>
+                                </h6>
+                            </footer>
+                        </div>
+                    </article>
+                </a>
+            </li>
+        );
     }
 }
 
 ReactDOM.render(
-    <PostsList posts={posts}/>,
+    <main><PostsList posts={posts}/></main>,
     document.getElementById("content")
 );
