8721df1a266d51ee23380d250316ce7ab43d04bf
Maciej Laciak <maciej.laciak@gmail.com>
2016-01-31 16:27:23 +0000

    Configures browserify to transform javascript code from ES2015 using babel.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,38 @@
 import gulp from "gulp";
+import buffer from "vinyl-buffer";
+import source from "vinyl-source-stream";
+import browserify from "browserify";
+import babelify from "babelify";
 
-gulp.task("default");
+
+const paths = (() => {
+    const
+        srcRoot = "./src",
+        distRoot = "./dist",
+        js = "js",
+        appFile = "app.js";
+    return {srcRoot, distRoot, js, appFile};
+})();
+
+const options = {
+    browserify: {
+        basedir: paths.srcRoot,
+        debug: true
+    }
+};
+
+gulp.task("default", () => {
+    browserify(
+        `${paths.js}/${paths.appFile}`,
+        options.browserify
+    )
+        .transform(babelify)
+        .bundle()
+        .on(
+            "error",
+            (err) => console.error(err)
+        )
+        .pipe(source(`${paths.js}/${paths.appFile}`))
+        .pipe(buffer())
+        .pipe(gulp.dest(`${paths.distRoot}`));
+});
