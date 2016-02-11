0f5ce42558ac1feb2ede9124d20d097c5cd2a6db
Maciej Laciak <maciej.laciak@gmail.com>
2016-02-11 23:46:13 +0000

    Adds a debug logging.

--- src/js/Store.js
+++ src/js/Store.js
@@ -1,9 +1,28 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk"
+import Minilog from "minilog";
 import app from "./reducers/App";
 import * as ActionCreators from "./actions/ActionCreators";
 import * as ArticlesMock from "./mocks/ArticlesMock";
 
-const store = createStore(app);
+Minilog.enable();
+var log = Minilog("app");
+
+
+const logger = store => next => action => {
+    log.debug("dispatching action: ", action);
+    const result = next(action);
+    log.debug("new state: ", store.getState());
+    return result;
+};
+
+const store = createStore(
+    app,
+    applyMiddleware(
+        thunkMiddleware,
+        logger
+    )
+);
 
 
 ArticlesMock.articles.forEach((article) => {
