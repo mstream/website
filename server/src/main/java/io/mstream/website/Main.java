fdd783d11801d8c9ae65651bcfb6dcc7cb3d6a14
Maciej Laciak <maciej.laciak@gmail.com>
2016-03-06 12:10:24 +0000

    Adds the server playbook role which creates a configuration file.

--- server/src/main/java/io/mstream/website/Main.java
+++ server/src/main/java/io/mstream/website/Main.java
@@ -3,23 +3,39 @@ package io.mstream.website;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.mstream.website.config.DatabaseConfig;
 import io.mstream.website.routes.RouteChain;
 import ratpack.server.RatpackServer;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
         RatpackServer.start(server ->
                 server
+                        .serverConfig(config ->
+                                config
+                                        .yaml(
+                                                System.getProperty("conf.file")
+                                        )
+                                        .require(
+                                                "/database",
+                                                DatabaseConfig.class
+                                        )
+                        )
                         .registryOf(registry ->
                                 registry
                                         .add(
                                                 ObjectMapper.class,
                                                 new ObjectMapper()
-                                                        .registerModule(new JavaTimeModule())
+                                                        .registerModule(
+                                                                new JavaTimeModule()
+                                                        )
                                         )
                         )
-                        .handlers(new RouteChain())
+                        .handlers(
+                                new RouteChain()
+                        )
         );
     }
 
--- server/src/main/java/io/mstream/website/routes/ApiChain.java
+++ server/src/main/java/io/mstream/website/routes/ApiChain.java
@@ -1,7 +1,9 @@
 package io.mstream.website.routes;
 
+import io.mstream.website.config.DatabaseConfig;
 import ratpack.func.Action;
 import ratpack.handling.Chain;
+import ratpack.jackson.Jackson;
 
 
 public class ApiChain implements Action<Chain> {
@@ -9,6 +11,7 @@ public class ApiChain implements Action<Chain> {
     @Override
     public void execute(Chain chain) throws Exception {
         chain
+                .prefix("config", new ConfigChain())
                 .prefix("articles", new ArticlesChain());
     }
 }
